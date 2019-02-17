ffa7de135d549d28169557c17e5585ff67f7817c
Doofitator <ashley@sharkey.id.au>
2019-02-17 21:08:47 +1100

    Added color option. I kind of brutally murdered the code, but it works.

--- scripts.js
+++ scripts.js
@@ -44,11 +44,29 @@ function GetClock() {
 }
   
   function addLink() {
-      document.getElementById("links").innerHTML = '<p>Add Shortcut Link</p><input type="text" id="linkName" class="linkText" placeholder="Name"/><br><input type="text" id="linkUrl" class="linkText" placeholder="Address"/><br><input class="linkText" type="button" value=" Save " onclick="saveLink()" />';
+      document.getElementById("links").innerHTML = '<p>Add Shortcut Link</p><input type="text" id="linkName" class="linkText" placeholder="Name"/><br><input type="text" id="linkUrl" class="linkText" placeholder="Address"/><br><input class="linkText" type="button" value=" Save " id="btn" onclick="saveLink()" />';
+      document.getElementById('linkName').style.color = localStorage.getItem('color');
+      document.getElementById('linkName').style.borderColor = localStorage.getItem('color');
+      document.getElementById('linkUrl').style.color = localStorage.getItem('color');
+      document.getElementById('linkUrl').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.color = localStorage.getItem('color');
   }
 
   function editWallpaper() {
-      document.getElementById("links").innerHTML = '<p>Edit Wallpaper URL</p><input type="text" id="wallpaperURL" class="linkText" placeholder="Image URL"/><br><input class="linkText" type="button" value=" Save " onclick="saveWallpaper()" />';
+      document.getElementById("links").innerHTML = '<p>Edit Wallpaper URL</p><input type="text" id="wallpaperURL" class="linkText" placeholder="Image URL"/><br><input class="linkText" id="btn" type="button" value=" Save " onclick="saveWallpaper()" />';
+      document.getElementById('wallpaperURL').style.color = localStorage.getItem('color');
+      document.getElementById('wallpaperURL').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.color = localStorage.getItem('color');
+  }
+
+  function editText() {
+      document.getElementById("links").innerHTML = '<p>Edit Text Color</p><input type="text" id="TextColor" class="linkText" placeholder="Color"/><br><input class="linkText" id="btn" type="button" value=" Save " onclick="saveText()" />';
+      document.getElementById('TextColor').style.color = localStorage.getItem('color');
+      document.getElementById('TextColor').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.borderColor = localStorage.getItem('color');
+      document.getElementById('btn').style.color = localStorage.getItem('color');
   }
         
   function saveLink() {
@@ -75,16 +93,40 @@ function GetClock() {
       
       location.reload();
   }
+
+  function saveText() {
+      var color = document.getElementById('TextColor').value;
+      
+      localStorage.setItem('color', color);
+      
+      location.reload();
+  }
   
   function displayBtns() {
       if (document.getElementById('links').innerHTML.includes('addLink')) {
-          var end = '<img src="cog.png" height="16px" onclick="displayBtns()" />';
+          var end = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path id="cog" d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z" onclick="displayBtns()"></svg>';
           document.getElementById('links').innerHTML= buildLinkString(end);
+          
+    
+    if (localStorage.getItem('color') != null) {
+        document.getElementById('cog').style.fill = localStorage.getItem('color');
+    } else {
+        document.getElementById('cog').style.fill = "white";
+    }
+    
       } else {
-          var end = '<img src="cog.png" height="16px" onclick="displayBtns()" />';
-          var btns = ' | <a onclick="addLink()" style="text-decoration:underline;cursor:pointer;">Add</a> <a onclick="killLinks()" style="text-decoration:underline;cursor:pointer;">Clear</a> <a onclick="editWallpaper()" style="text-decoration:underline;cursor:pointer;">Wallpaper</a> ';
+          var end = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path id="cog" d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z" onclick="displayBtns()"></svg>';
+          var btns = ' | <a onclick="addLink()" style="text-decoration:underline;cursor:pointer;">Add</a> <a onclick="killLinks()" style="text-decoration:underline;cursor:pointer;">Clear</a> <a onclick="editWallpaper()" style="text-decoration:underline;cursor:pointer;">Wallpaper</a> <a onclick="editText()" style="text-decoration:underline;cursor:pointer;">Color</a> ';
         var input = btns + end;
-        document.getElementById('links').innerHTML= buildLinkString(input);  
+        document.getElementById('links').innerHTML= buildLinkString(input);
+        
+    
+    if (localStorage.getItem('color') != null) {
+        document.getElementById('cog').style.fill = localStorage.getItem('color');
+    } else {
+        document.getElementById('cog').style.fill = "white";
+    }
+    
     }
     
 }
@@ -112,12 +154,21 @@ function buildLinkString(input) {
 }
         
 function loadShortcuts() {
-    var end = '<img src="cog.png" height="16px" onclick="displayBtns()" />';
+    var end = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path id="cog" d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z" onclick="displayBtns()"></svg>';
     
     
     document.getElementById('links').innerHTML= buildLinkString(end);
     
+    
+    if (localStorage.getItem('color') != null) {
+        document.getElementById('cog').style.fill = localStorage.getItem('color');
+    } else {
+        document.getElementById('cog').style.fill = "white";
+    }
+    
     loadWallpaper()
+    
+    loadColor()
     
     GetClock();
 }
@@ -125,6 +176,22 @@ function loadShortcuts() {
 function loadWallpaper() {
     if (localStorage.getItem('wallpaper') != null) {
         document.getElementById('background').style.backgroundImage = "url('" + localStorage.getItem('wallpaper') + "')";
+    }
+}
+
+function loadColor() {
+    if (localStorage.getItem('color') != null) {
+        document.getElementById('search').style.color = localStorage.getItem('color');
+        document.getElementById('queryTerm').style.color = localStorage.getItem('color');
+        document.getElementById('cog').style.fill = localStorage.getItem('color');
+        
+        var inputs = document.getElementsByTagName("div");
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].style.color = localStorage.getItem('color');
+        }
+        
+    } else {
+        document.getElementById('cog').style.fill = "white";
     }
 }
 
